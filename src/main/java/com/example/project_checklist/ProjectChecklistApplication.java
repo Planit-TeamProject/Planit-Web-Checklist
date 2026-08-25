bdbd363ddb7689765340fec039b685c8642dd05c
Siwoo Yoo <dbtldn0263@gmail.com>
2026-08-25 09:22:11 +0900

    first commit

---
+++ src/main/java/com/example/project_checklist/ProjectChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectChecklistApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectChecklistApplication.class, args);
+	}
+	
+}
